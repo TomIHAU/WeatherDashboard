d96aad09520f785dc5d720aa0f5788d537f5b3a0
TauH <thaywood988@gmail.com>
2021-09-24 16:26:25 +1000

    proving links are working

--- script.js
+++ script.js
@@ -5,41 +5,33 @@ function getInfo(event){
     event.preventDefault();
     var searchInputVal = document.querySelector("#searchInput").value.trim();
 
+    console.log(searchInputVal);
+
     if (!searchInputVal){
         console.log("please search for a city")
         return;
     }
 
-    var findLL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInputVal + "&appid=43ae064b43fdce4552702399802b6511&units=metric"
-    console.log(findLL);
+    var searchApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInputVal + "&appid=43ae064b43fdce4552702399802b6511&units=metric"
+    console.log(searchApi);
 
-    fetch(findLL)
+    fetch(searchApi)
     .then(function(response){
         if (!response){
             throw(error);
         }
         return response.json()
     })
-    .then(function(LLApi){
-        
-        var location = "https://api.openweathermap.org/data/2.5/onecall?lat=" + LLApi.city.coord.lat + "&lon=" +LLApi.city.coord.lon  + "&appid=43ae064b43fdce4552702399802b6511&units=metric"
-        console.log(location) 
-        fetch(location)
-        .then(function(response){
-            if (!response){
-                throw(error);
-            }
-            return response.json()
-        }).then(function(Api){
-                console.log(Api)
-            // for(let i = 0; i < Api.list.length; i += 8){
-            // console.log(moment.unix(Api.list[i].dt).format("LLLL"))}
-        })
-
-
-
-        
-    
+    .then(function(Api){
+        console.log(Api)
+        for(let i = 0; i < Api.list.length; i += 8){
+            console.log(Api.list[i].weather[0].icon)
+        console.log("http://openweathermap.org/img/wn/" + Api.list[i].weather[0].icon + "@2x.png")
+        console.log(moment.unix(Api.list[i].dt).format("DD/MM/YYYY"))
+        console.log(Math.round(Api.list[i].main.temp))
+        console.log(Api.list[i].main.humidity)
+        console.log(Math.round(Api.list[i].wind.speed * 3.6))
+    }
     })
 }
 
