886ac2d69fc4cabd21e9a648437a9312cc376c19
TauH <thaywood988@gmail.com>
2021-10-09 13:12:29 +1100

    added local storage updated ccs a little

--- script.js
+++ script.js
@@ -1,113 +1,115 @@
 var searchFormEl = document.querySelector("#searchForm");
 var searchHistoryEl = document.querySelector(".searchHistory");
 var resultsEl = document.querySelector(".results");
-var oldBtn = document.querySelector("ul")
+var oldBtn = document.querySelector("ul");
+var oldSearches = JSON.parse(localStorage.getItem("weatherDashSearches"));
 var pastSearch = [];
 var pastResult = [];
 
-function getInfo(event){
-    event.preventDefault();
-    var searchInputVal = document.querySelector("#searchInput").value.trim();
-    
+function getInfo(event) {
+  event.preventDefault();
+  var searchInputVal = document.querySelector("#searchInput").value.trim();
 
-    if (!searchInputVal){
-        console.log("please search for a city")
-        return;
-    }
+  if (!searchInputVal) {
+    console.log("please search for a city");
+    return;
+  }
 
-    var searchApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInputVal + "&appid=43ae064b43fdce4552702399802b6511&units=metric"
-    
-    fetch(searchApi)
-    .then(function(response){
-        if (!response.ok){
-            throw(error);
-        }
-        return response.json()
+  gettingResults(searchInputVal);
+}
+
+function gettingResults(searchInput) {
+  var searchApi =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    searchInput +
+    "&appid=43ae064b43fdce4552702399802b6511&units=metric";
+
+  fetch(searchApi)
+    .then(function (response) {
+      if (!response.ok) {
+        throw error;
+      }
+      return response.json();
     })
-    .then(function(Api){ 
-        pastSearch.push(searchInputVal);
-        pastResult.push(Api)
-        displayResults(Api);
+    .then(function (Api) {
+      pastSearch.push(searchInput);
+      pastResult.push(Api);
+      displayResults(Api);
     })
-    .catch(function(error){
-        console.log(error);
-        return;
-      })
+    .catch(function (error) {
+      console.log(error);
+      return;
+    });
 }
 
-function displayResults(Api){
-    
+function displayResults(Api) {
+  resultsEl.innerHTML = "";
+  for (let i = 0; i < Api.list.length; i += 8) {
+    var weatherCardEl = document.createElement("div");
+    weatherCardEl.classList = "infoCard";
 
-    resultsEl.innerHTML = "";
-    for(let i = 0; i < Api.list.length; i += 8){
+    var dayEl = document.createElement("div");
+    dayEl.innerText = moment.unix(Api.list[i].dt).format("dddd");
+    weatherCardEl.appendChild(dayEl);
 
-        var weatherCardEl = document.createElement('div')
-        weatherCardEl.classList = "infoCard";
+    var dateEl = document.createElement("div");
+    dateEl.innerText = moment.unix(Api.list[i].dt).format("DD/MM/YYYY");
+    weatherCardEl.appendChild(dateEl);
 
-        var dayEl = document.createElement("div");
-        dayEl.innerText = moment.unix(Api.list[i].dt).format("dddd");
-        weatherCardEl.appendChild(dayEl)
+    var icon = document.createElement("img");
+    icon.src =
+      "http://openweathermap.org/img/wn/" +
+      Api.list[i].weather[0].icon +
+      "@2x.png";
+    weatherCardEl.appendChild(icon);
 
-        var dateEl = document.createElement("div");
-        dateEl.innerText = moment.unix(Api.list[i].dt).format("DD/MM/YYYY");
-        weatherCardEl.appendChild(dateEl)
+    var tempEl = document.createElement("div");
+    tempEl.innerText = "Temp: " + Math.round(Api.list[i].main.temp) + " °C";
+    weatherCardEl.appendChild(tempEl);
 
-        var icon = document.createElement("img")
-        icon.src = "http://openweathermap.org/img/wn/" + Api.list[i].weather[0].icon + "@2x.png"
-        weatherCardEl.appendChild(icon)
+    var windSpeedEl = document.createElement("div");
+    windSpeedEl.innerText =
+      "Wind: " + Math.round(Api.list[i].wind.speed * 3.6) + " km/h";
+    weatherCardEl.appendChild(windSpeedEl);
 
-        var tempEl = document.createElement("div");
-        tempEl.innerText = "Temp: " + Math.round(Api.list[i].main.temp) + " °C";
-        weatherCardEl.appendChild(tempEl)
-        
-        var windSpeedEl = document.createElement("div");
-        windSpeedEl.innerText ="Wind: " + Math.round(Api.list[i].wind.speed * 3.6) + " km/h"
-        weatherCardEl.appendChild(windSpeedEl)
+    var humidityEl = document.createElement("div");
+    humidityEl.innerText = "Humidity: " + Api.list[i].main.humidity + " %";
+    weatherCardEl.appendChild(humidityEl);
 
-        var humidityEl = document.createElement("div");
-        humidityEl.innerText = "Humidity: "+ Api.list[i].main.humidity +" %"
-        weatherCardEl.appendChild(humidityEl)
-        
-        resultsEl.appendChild(weatherCardEl)
-        
-    }
-    createList();
+    resultsEl.appendChild(weatherCardEl);
+  }
+  createList();
 }
 
-function displayOld(event){
-    var element = event.target;
-    if (element.matches("button") === true) {
-        
-        var index = element.getAttribute("data-index");
-        console.log("hello");
-    console.log(index)
-    console.log(pastResult[index])
-    displayResults(pastResult[index]);
-    }
+function displayOld(event) {
+  var element = event.target;
+  if (element.matches("button") === true) {
+    var thisSearch = element.innerHTML;
+
+    gettingResults(thisSearch);
+  }
 }
 
-function createList(){
-    searchHistoryEl.innerHTML = "";
-    if(pastSearch.length > 5){
-        console.log("hello")
-        pastSearch.shift();
-        pastResult.shift();
-    }
-    console.log(pastResult);
-    console.log(pastSearch);
+function createList() {
+  searchHistoryEl.innerHTML = "";
+  if (oldSearches != undefined) {
+    pastSearch = oldSearches;
+  }
+  if (pastSearch.length > 5) {
+    pastSearch.shift();
+  }
 
-    for(let i = 0; i < pastResult.length; i++){
-        
-        var button = document.createElement("button")
-        button.innerText = pastSearch[i];
-        button.setAttribute("data-index", i);
-        
-        var li = document.createElement("li");
-        li.appendChild(button)
+  for (let i = 0; i < pastSearch.length; i++) {
+    var button = document.createElement("button");
+    button.innerText = pastSearch[i];
 
-        searchHistoryEl.appendChild(li);
-    }
+    var li = document.createElement("li");
+    li.appendChild(button);
+
+    searchHistoryEl.appendChild(li);
+  }
+  localStorage.setItem("weatherDashSearches", JSON.stringify(pastSearch));
 }
-
+createList();
 oldBtn.addEventListener("click", displayOld);
 searchFormEl.addEventListener("submit", getInfo);
